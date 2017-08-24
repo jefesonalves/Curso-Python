4820037f44ca934131e2ed51b7065169ab7630a2
Jefeson Alves <jefesonbezerra@gmail.com>
2017-08-24 13:24:50 -0300

    Update and rename desafio19.py to desafio19 e 20.py

--- desafio19 e 20.py
+++ desafio19 e 20.py
@@ -1,19 +1,22 @@
-# Desafio 19 do Curso em Vídeo
+# Desafio 19 e 20(item 4 do menu) do Curso em Vídeo
 import random
 
 registro = []
 
 while True:
-    print('\nMenu de opções\n1.Cadastrar\n2.Consultar\n3.Sortear\n4.Sair\n ')
+    print('\nMenu de opções\n1.Cadastrar\n2.Consultar\n3.Sortear\n4.Ordenar\n5.Sair\n ')
     opcao = input('Escolha a opção: ')
     if opcao == '1':
         registro.append(input('Digite o nome: \n'))
         print('\nGravado com sucesso!')
     elif opcao == '2':
-        print('\nLista dos registros {} '.format(registro))
+        print('\nLista dos registros: {} '.format(registro))
     elif opcao == '3':
         print('\nNome sorteado: {}'.format(random.choice(registro)))
     elif opcao == '4':
+        registro.sort()
+        print('Lista ordenada: {}'.format(registro))
+    elif opcao == '5':
         print('\nVocê saiu do sistema...')
         exit()
     else:
