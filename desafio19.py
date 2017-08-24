3572b587e51584792611dc380d7c32d0c51e33fe
Jefeson Alves <jefesonbezerra@gmail.com>
2017-08-24 11:54:04 -0300

    Add files via upload

---
+++ desafio19.py
@@ -0,0 +1,20 @@
+# Desafio 19 do Curso em Vídeo
+import random
+
+registro = []
+
+while True:
+    print('\nMenu de opções\n1.Cadastrar\n2.Consultar\n3.Sortear\n4.Sair\n ')
+    opcao = input('Escolha a opção: ')
+    if opcao == '1':
+        registro.append(input('Digite o nome: \n'))
+        print('\nGravado com sucesso!')
+    elif opcao == '2':
+        print('\nLista dos registros {} '.format(registro))
+    elif opcao == '3':
+        print('\nNome sorteado: {}'.format(random.choice(registro)))
+    elif opcao == '4':
+        print('\nVocê saiu do sistema...')
+        exit()
+    else:
+        print('Opção inválida, digite opções entre 1 e 4')
